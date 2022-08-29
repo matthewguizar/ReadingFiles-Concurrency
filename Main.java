e6d7f48e5ff2455a4c2b0055834d189929e4dc47
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-29 08:14:19 -0700

    removing locks. adding atomic integers

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 
@@ -14,16 +15,18 @@ public class Main {
  
     static String[] files = new String[] { "data/sales1.csv", "data/sales2.csv", "data/sales3.csv"};
    
-    static int sampleSize = 0;
-    static int quantitySold = 0;
+    // static int sampleSize = 0;
+    static AtomicInteger sampleSize = new AtomicInteger(0);
+    // static int quantitySold = 0;
+    static AtomicInteger quantitySold = new AtomicInteger(0);
 
 
     public static void main(String[] args) throws Exception {
         CountDownLatch latch = new CountDownLatch(3);
-        ReentrantLock lock = new ReentrantLock();
+        // ReentrantLock lock = new ReentrantLock();
         ExecutorService executor = Executors.newFixedThreadPool(3);
        for (String string : files) {
-            executor.submit(() -> increment(string, latch, lock));
+            executor.submit(() -> increment(string, latch));
        }
      
         Scanner scan = new Scanner(System.in);
@@ -40,17 +43,17 @@ public class Main {
      
     }
 
-    public static void increment(String file, CountDownLatch latch, ReentrantLock lock){
+    public static void increment(String file, CountDownLatch latch){
         try {
             Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(file).toURI());
             Files.lines(path)
                 .skip(1)
                 .mapToInt((line) ->Integer.parseInt(line.split(",")[2]))
                 .forEach((quantity) -> {
-                    lock.lock();
-                    sampleSize++;
-                    quantitySold += quantity;
-                    lock.unlock();
+                    // lock.lock();
+                    sampleSize.addAndGet(1);
+                    quantitySold.addAndGet(quantity);
+                    // lock.unlock();
                 });
         } catch (URISyntaxException e) {
             System.out.println(e.getMessage());
